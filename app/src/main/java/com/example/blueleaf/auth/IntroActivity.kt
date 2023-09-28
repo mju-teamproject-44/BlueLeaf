41cecf49113e1c5f0f29b7d884d1a6e19e261136
gdrffg <gdrffg@gmail.com>
2023-09-28 16:25:06 +0900

    FIX: Splash 화면에서 Intro로 넘어가는 로직 구현
    Splash -> Intro로 넘어가는 로직 구현
    디자인 미반영

--- app/src/main/java/com/example/blueleaf/auth/IntroActivity.kt
+++ app/src/main/java/com/example/blueleaf/auth/IntroActivity.kt
@@ -1,12 +1,12 @@
-package com.example.blueleaf
+package com.example.blueleaf.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.blueleaf.R
 
-class MainActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_intro)
     }
 }
