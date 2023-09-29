759ebbf43a4cbe62519b7feb79b6dbd2eb69433f
gdrffg <gdrffg@gmail.com>
2023-09-29 15:54:45 +0900

    FEAT: Main 화면 상단 바 초기 UI
    Main 화면 상단 바 초기 UI

--- app/src/main/java/com/example/blueleaf/SplashActivity.kt
+++ app/src/main/java/com/example/blueleaf/SplashActivity.kt
@@ -5,15 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.blueleaf.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        auth = Firebase.auth
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        // 이미 로그인 된 유저가 아니면 IntroActivity로 이동
+        if(auth.currentUser?.uid == null) {
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+
+        }
+        // 이미 로그인 된 유저이면 Main으로 이동
+        else {
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
+
     }
 }
