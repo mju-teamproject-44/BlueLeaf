00ed07266fa3db273ffdaaf1fee9f7c20fea4c8f
yyyeee1108 <103637869+yyyeee1108@users.noreply.github.com>
2023-10-10 22:37:39 +0900

    FEAT: 게시판 글쓰기 업로드 기능 추가

--- app/src/main/java/com/example/blueleaf/board/BoardWriteActivity.kt
+++ app/src/main/java/com/example/blueleaf/board/BoardWriteActivity.kt
@@ -2,11 +2,42 @@ package com.example.blueleaf.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.blueleaf.R
+import com.example.blueleaf.contentsList.BookmarkModel
+import com.example.blueleaf.databinding.ActivityBoardWriteBinding
+import com.example.blueleaf.utils.FBAuth
+import com.example.blueleaf.utils.FBRef
 
 class BoardWriteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBoardWriteBinding
+
+    private val TAG = BoardWriteActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_write)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_board_write)
+
+        binding.writeBtn.setOnClickListener{
+            val title = binding.titleArea.text.toString()
+            val content = binding.contentArea.text.toString()
+            val uid = FBAuth.getUid()
+            val time = FBAuth.getTime()
+
+            Log.d(TAG,title)
+            Log.d(TAG,content)
+
+            FBRef.boardRef
+                .push()
+                .setValue(BoardModel(title,content,uid,time))
+
+            Toast.makeText(this,"게시글 입력 완료",Toast.LENGTH_LONG).show()
+
+            finish()
+
+        }
     }
 }
