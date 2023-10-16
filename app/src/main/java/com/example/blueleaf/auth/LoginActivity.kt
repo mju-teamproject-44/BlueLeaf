22814b457a7b2da40017ef39a3415e2bf2eb1e8e
JinhyunKim <144112133+Jinhyun217@users.noreply.github.com>
2023-10-17 00:44:43 +0900

    FEAT : 로그인 과정에서 FB에 유저 데이터 추가 기능 구현
    변경사항은 아래와 같습니다.
    - 회원가입시 유저 데이터 DB에 기록
    - 비회원로그인시 유저 데이터 DB에 기록
    - (임시 코드) DB에 유저 데이터가 없는 경우, 임시 데이터 추가

--- app/src/main/java/com/example/blueleaf/auth/LoginActivity.kt
+++ app/src/main/java/com/example/blueleaf/auth/LoginActivity.kt
@@ -7,14 +7,20 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.blueleaf.MainActivity
 import com.example.blueleaf.R
+import com.example.blueleaf.contentsList.UserModel
 import com.example.blueleaf.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+
+    //Database Reference
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +40,25 @@ class LoginActivity : AppCompatActivity() {
                         startActivity(intent)
 
                         Toast.makeText(this,"로그인 성공", Toast.LENGTH_LONG).show()
+
+                        // * 기존 유저 데이터(FB 연동이 안되어 있는 데이터) 임시 추가용.
+                        database = Firebase.database.reference
+                        val userUID = Firebase.auth.currentUser?.uid
+
+
+                        if(userUID!=null){
+                            database.child("users").child(userUID!!).child("userName").get().addOnSuccessListener {
+                                if(it.value == null){
+                                    val userData = UserModel("사용자", "NoProfile", email)
+                                    database.child("users").child(userUID!!).setValue(userData)
+                                }
+                            }.addOnFailureListener {
+                                //
+                            }
+                        }else{
+                            Toast.makeText(this,"uid fail", Toast.LENGTH_LONG).show()
+                        }
+
                     } else {
                         Toast.makeText(this,"로그인 실패", Toast.LENGTH_LONG).show()
 
