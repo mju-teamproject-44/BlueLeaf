2efd3fe3b4510bf538251ddb6250282cd1faadf8
gdrffg <gdrffg@gmail.com>
2023-10-17 17:30:37 +0900

    Fix: resolve conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,10 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.firebase:firebase-database-ktx")
+
+    // Storage & ImageView
     implementation("com.google.firebase:firebase-storage-ktx")
-    implementation("com.firebaseui:firebase-ui-storage:7.2.0")
+    // implementation("com.firebaseui:firebase-ui-storage:7.2.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
